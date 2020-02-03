d2c80907f87219305d4f4e92f2baad59832844e4
ADMINISTRATOR <Mayankrathi33@gmail.com>
2020-02-03 11:13:08 +0530

    ADDED:Resigning conditon for win and loose  using while loop

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -6,10 +6,22 @@ BET=1
 
 #Variables
 stake=100
+win=0
+loose=0
 
-if [ $((RANDOM%2)) -eq 1 ]
-then 
-	echo "WIN BET"
-else
-	echo "Loose BET"
-fi
+WIN_MIN=$(($stake*50/100))
+WIN_MAX=$(($stake+$WIN_MIN))
+
+while(($stake >= WIN_MIN)) && (($stake <= WIN_MAX))
+do
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		((win++))
+		((stake++))
+		echo "Resign The Game value is 150"
+	else
+		((loose++))
+		((stake--))
+		echo "Resign The Game value less then 50"
+	fi 	
+done
